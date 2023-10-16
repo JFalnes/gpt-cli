fb38e6e6cac6d9343a10457e9b2cf2c3e66f3b4b
jfalnes <johannes@falnes.no>
2023-10-16 10:39:56 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,70 @@
+import click
+import openai
+import os
+import time
+import json
+from dotenv import load_dotenv
+from colorama import Fore, Style, init
+import click
+import openai
+import re
+
+# Initialize colorama
+init()
+
+load_dotenv()
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+def load_config():
+    with open('config.json', 'r') as f:
+        return json.load(f)
+
+
+@click.command()
+@click.option("--model", default="gpt-4", help="The name of the GPT model to use.")
+def chat(model):
+    """
+    This function initializes a chatbot using the OpenAI API and allows the user to interact with it through the command line.
+    
+    Args:
+    - model (str): The name of the GPT model to use. Default is "gpt-4".
+    """
+    config = load_config()
+    messages = config['messages']
+    while True:
+        user_message = click.prompt('', prompt_suffix=f'{Fore.BLUE}>>> {Fore.RESET}', type=str)
+        messages.append({"role": "user", "content": user_message})
+        
+        # Record the time before the request is sent
+        start_time = time.time()
+        
+        # Send a ChatCompletion request with stream=True
+        response_gen = openai.ChatCompletion.create(
+                model=model,
+                messages=messages,
+                **config['response_params']
+            )
+        
+        # Create variables to collect the stream of chunks
+        collected_chunks = []
+        collected_messages = []        
+        # Print the assistant's response label
+        print(f'{Fore.GREEN}GPT-{model[-1]}: ', end='', flush=True)
+        
+        # Iterate through the stream of events
+        for chunk in response_gen:
+            collected_chunks.append(chunk)  # Collect chunks here
+            chunk_message = chunk['choices'][0]['delta']  # Extract the message
+            collected_messages.append(chunk_message)  # Save the message
+            text_content = chunk_message.get('content', '')
+            print(text_content, end='', flush=True)
+        
+        print(f'{Fore.RESET}\n', end='')  # Print a newline at the end of the streaming text, reset color
+        
+        # Update the messages list with the assistant's response
+        full_reply_content = ''.join([m.get('content', '') for m in collected_messages])
+        messages.append({"role": "assistant", "content": full_reply_content})
+
+if __name__ == "__main__":
+    chat()
---
+++ readme.md
@@ -0,0 +1,59 @@
+# GPT-4 CLI Chatbot
+
+This project contains a command-line interface (CLI) chatbot powered by OpenAI's GPT-4 model. It allows users to interact with the GPT-4 model in a conversational manner directly from the terminal.
+
+## Features
+
+- Real-time interaction with the GPT-4 model.
+- Colored prompts for better user experience.
+- Ability to stream the model's response back to the user.
+- Configurable settings via an external JSON file.
+
+## Installation
+
+Ensure you have the necessary libraries installed using pip:
+
+```bash
+pip install openai click colorama python-dotenv
+```
+
+## Configuration
+1. Create a `.env` file in the project root and add your OpenAI API key like so:
+
+```plaintext
+OPENAI_API_KEY=your-api-key-here
+```
+
+2. Edit the `config.json` file in the project root with the following structure:
+
+```json
+{
+    "messages": [
+        {
+            "role": "system",
+            "content": "You are a helpful assistant."
+        }
+    ],
+    "response_params": {
+        "temperature": 0.3,
+        "stream": true
+    }
+}
+```
+
+- The `messages` array contains the initial messages to set the behavior of the model.
+- The `response_params` object contains parameters for the `openai.ChatCompletion.create` method to control the behavior of the model.
+
+## Usage
+
+Run the script using Python:
+
+```bash
+python main.py
+```
+
+You'll be greeted with a prompt (`>>>`). Here, you can type your message to the GPT-4 model. The model's response will be streamed back to you in real-time.
+
+## Contributing
+
+Feel free to fork this repository, make changes, and submit pull requests. Any contributions are welcome!
