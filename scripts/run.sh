c6cae5efb751cd3dce50d1dc9cdbfb3b2b347197
jfalnes <johannes@falnes.no>
2023-10-18 10:20:46 +0200

    Fixed run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-# Get the directory of the currently executing script
-DIR="$(dirname "$(readlink -f "$0")")"
+# Change to the parent directory
+cd "$(dirname "$0")/.."
+
+# Get the directory of the currently executing script (which is now the parent directory)
+DIR="$(pwd)"
 
 # Activate the virtual environment
 source "$DIR/.venv/bin/activate"
 
 # Run the Python script
-python "$DIR/src/main.py"
+python3 "$DIR/src/main.py"
 
 # Deactivate the virtual environment
 deactivate
